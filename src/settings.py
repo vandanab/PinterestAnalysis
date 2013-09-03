4209aadb4ea82331432c19777cce0359ee7ee0bc
Vandana <vandana@vandana-laptop.(none)>
2013-09-03 00:21:46 -0500

    users data in hdfs

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ Pinterest data crawler settings
 import os
 
 rex_local_base_dir = os.path.expanduser("~/PinterestAnalysis/data/results/%s/")
-chevron_base_dir = "/mnt/chevron/vbachani/pinterest/data/%s/"
+#chevron_base_dir = "/mnt/chevron/vbachani/pinterest/data/%s/"
+#f_users = rex_local_base_dir % "users" + "users.txt"
+hdfs_base_dir = "hdfs:///user/vbachani/PinterestAnalysis/data/%s/"
 f_users = rex_local_base_dir % "users" + "users.txt"
-
-
--- src/users_mr.py
+++ src/users_mr.py
@@ -5,7 +5,7 @@ map reduce runner for user related analysis of pinterest data
 '''
 from datetime import datetime
 from library.mrjobwrapper import runMRJob
-from settings import f_users, chevron_base_dir
+from settings import f_users, hdfs_base_dir
 from users import Users
 from src.utilities import fs
 
@@ -31,7 +31,7 @@ class UsersMRJobRunner(object):
                             datetime(2012, 12, 12), datetime(2013, 9, 1)
     UsersMRJobRunner.get_users(input_files_start_time,
                                input_files_end_time,
-                               chevron_base_dir % "users")
+                               hdfs_base_dir % "/input/users")
 
 if __name__ == '__main__':
   UsersMRJobRunner.run()
