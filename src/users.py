1c01c93ed91328ac51ef533598d900a9d826bf79
vardharaj1990 <vardharaj@melody.cs.tamu.edu>
2013-09-04 12:42:09 -0500

    merging changes

--- src/users.py
+++ src/users.py
@@ -16,9 +16,12 @@ class Users(ModifiedMRJob):
   mapper for finding valid users in pinterest data 
   """
   def mapper(self, key, line):
-    data = cjson.decode(line)
-    if len(data["boards"]) > 0 and int(data["boards_count"]) > 0:
-      yield key, data["user"]
+    try:
+      data = cjson.decode(line)
+      if len(data["boards"]) > 0 and "boards_count" in data and int(data["boards_count"]) > 0:
+        yield key, data["user"]
+    except:
+      print ""
 
 if __name__ == "__main__":
   Users.run()
--- src/users_mr.py
+++ src/users_mr.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from library.mrjobwrapper import runMRJob
 from settings import f_users, hdfs_base_dir
 from users import Users
-from src.utilities import fs
+from utilities import fs
 
 class UsersMRJobRunner(object):
   @staticmethod
@@ -21,17 +21,17 @@ class UsersMRJobRunner(object):
                                       input_folder),
              mrJobClassParams = {'job_id': 'as'},
              # uncomment when running on local
-             #args = [],
-             jobconf={'mapred.reduce.tasks':300, 'mapred.task.timeout': 86400000}
+             args = [],
+             jobconf={'mapred.reduce.tasks':300, 'mapred.task.timeout': 8640000}
     )
   
   @staticmethod
   def run():
     input_files_start_time, input_files_end_time = \
-                            datetime(2012, 12, 12), datetime(2013, 9, 1)
+                            datetime(2012, 12, 1), datetime(2013, 9, 1)
     UsersMRJobRunner.get_users(input_files_start_time,
                                input_files_end_time,
-                               hdfs_base_dir % "/input/users")
+                               hdfs_base_dir % "input/users")
 
 if __name__ == '__main__':
   UsersMRJobRunner.run()
